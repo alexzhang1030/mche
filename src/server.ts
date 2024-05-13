6860bdff0a72f02acc2e1c91049d14a8097a1502
Alex <alexzhang1030@foxmail.com>
2024-05-13 15:32:21 +0800

    feat: improve server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { jsonStringify, withSign } from 'wshe/server'
+import { getHeartbeatResponse, isHeartbeatRequestParsed, jsonStringify, withSign } from 'wshe/server'
 
 export function buildMetaPayload<T extends Record<string, any>>(event: keyof T, data: T[keyof T]) {
   return withSign(jsonStringify({
@@ -7,4 +7,47 @@ export function buildMetaPayload<T extends Record<string, any>>(event: keyof T, 
   }))
 }
 
-export * from 'wshe/server'
+enum MetaEventType {
+  Open = 'open',
+  Close = 'close',
+
+  Register = 'register',
+  RegisterAccept = 'register-accept',
+}
+
+export type MetaEventTypePayload = {
+  [K in MetaEventType]: any
+}
+
+export function withHandleMetaEvent() {
+  return {
+    // heartbeat...
+    isHeartbeatRequestParsed,
+    getHeartbeatResponse,
+    // register....
+    isMetaRegisterEvent(event: any) {
+      return event.event === MetaEventType.Register
+    },
+    getMetaRegisterResponse(roomId: string, userId: string) {
+      return buildMetaPayload<MetaEventTypePayload>(MetaEventType.Open, {
+        roomId,
+        data: [{ id: userId }],
+      })
+    },
+    getMetaRegisterAcceptResponse(roomId: string, userId: string) {
+      return buildMetaPayload<MetaEventTypePayload>(MetaEventType.RegisterAccept, {
+        roomId,
+        id: userId,
+      })
+    },
+    // close...
+    getMetaCloseResponse(roomId: string, userId: string) {
+      return buildMetaPayload<MetaEventTypePayload>(MetaEventType.Close, {
+        roomId,
+        data: {
+          id: userId,
+        },
+      })
+    },
+  }
+}
