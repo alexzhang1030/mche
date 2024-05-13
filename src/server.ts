4a89b745ddb165ba86e1d6fddf6a450ceb8caf28
Alex <alexzhang1030@foxmail.com>
2024-05-13 15:35:28 +0800

    feat: improve server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { getHeartbeatResponse, isHeartbeatRequestParsed, jsonStringify, withSign } from 'wshe/server'
+import { getHeartbeatResponse, isHeartbeatRequestParsed, jsonStringify, tryToParseRawMessage, withSign } from 'wshe/server'
 
 export function buildMetaPayload<T extends Record<string, any>>(event: keyof T, data: T[keyof T]) {
   return withSign(jsonStringify({
@@ -21,6 +21,9 @@ export type MetaEventTypePayload = {
 
 export function withHandleMetaEvent() {
   return {
+    tryParseMetaEvent(event: any) {
+      return tryToParseRawMessage(event)
+    },
     // heartbeat...
     isHeartbeatRequestParsed,
     getHeartbeatResponse,
